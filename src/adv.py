bb6641f4868e2905a63c012a0e6e31f624a8ed15
Cai Nowicki <cai.nowicki@gmail.com>
2020-09-09 00:27:58 -0400

    input parser complete, player can move around

--- src/adv.py
+++ src/adv.py
@@ -4,17 +4,17 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room("Outside Cave Entrance",
+                    "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow passage': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
@@ -22,18 +22,17 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-for k,v in room.items():
-    v = Room(name=k, description=v)
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
+room['foyer'].e_to = room['narrow passage']
 room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['narrow passage'].w_to = room['foyer']
+room['narrow passage'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow passage']
+
 
 #
 # Main
@@ -58,4 +57,23 @@ def playgame():
     player = Player(name=player_name)
     current_input = ""
     while current_input is not "q":
-        print(player)
+        current_room = player.current_room
+        print(room[current_room.lower()])
+        current_input = input("Where do you want to go now?")
+        if current_input == "q":
+            break
+        next_move = room[current_room.lower()].make_move(current_input)
+        if next_move is None:
+            print("There's nowhere to go in that direction. Try something else.")
+            continue
+        elif next_move is False:
+            print("That's not a valid move. Enter n, s, e, or w, or enter q to quit.")
+            continue
+        else:
+            room_name = next_move.name
+            player.current_room = room[room_name.lower()].name
+
+
+if __name__ == "__main__":
+    playgame()
+
